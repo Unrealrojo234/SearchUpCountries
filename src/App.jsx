0d5786cb688d7eb94889823abc2562459ea1ed4b
Unrealrojo234 <otienoryan812@gmail.com>
2024-07-14 23:41:55 -0700

    Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header"
 import Navigation from "./Navigation"
+import Footer from "./Footer"
 
 const App = ()=>{
   return(
     <>
         <Navigation/>
         <Header/>
+        <Footer/>
     </>
   )
 }
